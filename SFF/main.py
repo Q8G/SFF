41e7bfafe42b0f0a736efb79af1c3296708ebc45
L <74691792+Q8G@users.noreply.github.com>
2026-08-20 19:44:16 +0300

    SFF

--- SFF/main.py
+++ SFF/main.py
@@ -3,12 +3,12 @@ from colorama import init, Fore
 init()
 os.system("title Soul Files Finder")
 os.system('cls' if os.name == 'nt' else 'clear')
-filename = input(Fore.YELLOW + "File Name: " + Fore.RESET)
-search_path = "C:\\" if platform.system() == "Windows" else "/"
+fn = input(Fore.YELLOW + "File Name: " + Fore.RESET)
+sepa = "C:\\" if platform.system() == "Windows" else "/"
 found = False
-for root, dirs, files in os.walk(search_path):
-    if filename in files:
-        print(Fore.LIGHTGREEN_EX + f"[+] File found: {os.path.join(root, filename)}" + Fore.RESET)
+for root, dirs, files in os.walk(sepa):
+    if fn in files:
+        print(Fore.LIGHTGREEN_EX + f"[+] File found: {os.path.join(root, fn)}" + Fore.RESET)
         found = True
 if not found:
     print(Fore.RED + "[-] File not found." + Fore.RESET)
